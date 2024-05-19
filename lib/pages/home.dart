9603654116a386ad1f537176a6707624fb3b5909
akohinmiogbegbile <akohinmi90@gmail.com>
2024-05-19 02:04:52 +0100

    UI of the Onboard/Home pt 2

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tilomath/widget/support_widget.dart';
 
@@ -9,9 +10,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List categories = [
+    "images/headphone_icon.png",
+    "images/laptop.png",
+    "images/watch.png",
+    "images/TV.png"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xfff2f2f2),
       body: Container(
         margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
         child: Column(
@@ -44,8 +53,99 @@ class _HomeState extends State<Home> {
                 )
               ],
             ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                width: MediaQuery.of(context).size.width,
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Search Products",
+                      hintStyle: AppWidget.lightTextFieldStyle(),
+                      prefixIcon: Icon(Icons.search, color: Colors.black)),
+                )),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Categories", style: AppWidget.semiboldTextFieldStyle()),
+                Text("see all",
+                    style: TextStyle(
+                        color: Color(0xFFfd6f3e),
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold))
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              children: [
+                Container(
+                    height: 130,
+                    padding: EdgeInsets.all(20),
+                    margin: EdgeInsets.only(right: 20.0),
+                    decoration: BoxDecoration(
+                        color: Color(0xFFFd6f3e),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                        child: Text(
+                      "All",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold),
+                    ))),
+                Expanded(
+                  child: Container(
+                    height: 130,
+                    child: ListView.builder(
+                        padding: EdgeInsets.zero,
+                        itemCount: categories.length,
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return CategoryTile(image: categories[index]);
+                        }),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  String image;
+  CategoryTile({required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.only(right: 20.0),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            image,
+            height: 50,
+            width: 50,
+            fit: BoxFit.cover,
+          ),
+          Icon(Icons.arrow_forward)
+        ],
       ),
     );
   }
--- lib/widget/support_widget.dart
+++ lib/widget/support_widget.dart
@@ -10,4 +10,9 @@ class AppWidget {
     return TextStyle(
         color: Colors.black54, fontSize: 20.0, fontWeight: FontWeight.w500);
   }
+
+  static TextStyle semiboldTextFieldStyle() {
+    return TextStyle(
+        color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold);
+  }
 }
